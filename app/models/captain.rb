b0c9ad3fb8b2b328340698835c243e8207c7c1e5
Dani Lyn <dlyn1110@gmail.com>
2019-01-08 03:47:42 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,7 +6,7 @@ class Captain < ActiveRecord::Base
   end
 
   def self.sailors
-     joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+    joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
   def self.talented_seafarers
