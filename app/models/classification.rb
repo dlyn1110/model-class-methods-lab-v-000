78009b7665c718020f33b23de84f0cb20da457c7
Dani Lyn <dlyn1110@gmail.com>
2019-01-08 03:51:53 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,7 +2,7 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
-  def self.all
+  def self.my_all
     self.all
   end
 
